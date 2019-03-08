5946ab5a3f530b7b8ffa1e59865954caef2d2da7
Eleonora Della <2244079d@student.gla.ac.uk>
2019-03-08 12:29:04 +0000

    Revert "Merge branch 'master' into '56-data-screen-identifies-plate-as-balanced'"
    This reverts commit 0609e37506d29d1600fa0a1b6c173baae7d6b3fa

--- NutriPlotter/screens/main/DataScreen/DataScreen.js
+++ NutriPlotter/screens/main/DataScreen/DataScreen.js
@@ -15,11 +15,14 @@ import {
 
 import { Container, Header, Left, Body, Right,Button, Title, Content } from 'native-base';
 
+import SlidingUpPanel from 'rn-sliding-up-panel';
+//components creted by us:
+import {FoodList} from '../../../components/main/FoodList';
+import {PopUpMenu} from '../../../components/main/PopUpMenu';
 //stylesheets
 import styles from './styles';
 
-import {Svg} from 'expo';
-import {Slice} from '../../../components/main/Slice';
+import { PieChart } from 'react-native-svg-charts'
 
 Amplitude.initialize("8a8476a30e9af690b3dc1f1d7b637e4b")
 
@@ -254,77 +257,16 @@ export default class DataScreen extends React.Component {
 
     if (!this.state.done) {
       return (
-        <Container>
-          <Header style={{height: 100}}>
-            <Left>
-            <Button transparent onPress={() => {
-              back();
-              Amplitude.logEvent('Back button pressed from data screen');
-            }
-          }>
-            <Image source={require('./src/back.png')}
-                    style={{width: 35, height: 35}}/>
-        </Button>
-            </Left>
-            <Body>
-              <Title style={{width: 250}}>Nutritional Information</Title>
-            </Body>
-            <Right>
-              <Button transparent>
-
-              </Button>
-            </Right>
-          </Header>
-          <Content>
-            <View style={{height: 50, width: '100%', backgroundColor: 'gray', flexDirection: 'row', alignItems:'center'}}>
-              <Text style={{width: '50%', textAlign: 'center', fontSize: 25, fontFamily:'NunitoSans', color:'white'}}>Calories:</Text>
-              <Text style={{width: '50%', textAlign: 'center', fontSize: 25, fontFamily:'NunitoSans', color:'white'}}>800</Text>
-            </View>
-            <Svg
-                    width={160}
-                    style={styles.pieSVG}
-                    height={160}
-                    viewBox={`-100 -100 200 200`}
-                >
-              <Slice
-                  index={0}
-                  startAngle={0}
-                  endAngle={Math.PI}
-                  color={'#0d2f51'}
-                  key={'pie_shape_0'}
-              />
-              <Slice
-                  index={1}
-                  startAngle={Math.PI}
-                  endAngle={2*Math.PI}
-                  color={'#28BD8B'}
-                  key={'pie_shape_1'}
-              />
-          </Svg>
-          
-            {/**Pie chart comes here */}
-           <View style={{height: 50, width: '100%', backgroundColor: 'gray', flexDirection: 'row', alignItems:'center'}}>
-             <Text style={styles.text}>Protein:</Text>
-             <Text style={styles.text}>60g</Text>
-           </View>
-           <View style={{height: 50, width: '100%', backgroundColor: 'gray', flexDirection: 'row', alignItems:'center'}}>
-             <Text style={styles.text}>Carbohydrates:</Text>
-             <Text style={styles.text}>60g</Text>
-           </View>
-           <View style={{height: 50, width: '100%', backgroundColor: 'gray', flexDirection: 'row', alignItems:'center'}}>
-             <Text style={styles.text}>Fats:</Text>
-             <Text style={styles.text}>60g</Text>
-           </View>
-           <View style={{height: 50, width: '100%', backgroundColor: 'gray', flexDirection: 'row', alignItems:'center'}}>
-             <Text style={styles.text}>Sugar:</Text>
-             <Text style={styles.text}>60g</Text>
-           </View>
-           <View style={{height: 50, width: '100%', backgroundColor: 'gray', flexDirection: 'row', alignItems:'center'}}>
-             <Text style={styles.text}>Fibre:</Text>
-             <Text style={styles.text}>60g</Text>
-           </View>
-
-          </Content>
+        <Container style={styles.container}>
+          <View style={styles.indicator}>
+            <ActivityIndicator size="large" color="white" animating={true}/>
+          </View>
+          <View>
+            <Text  style={{width: '50%', textAlign: 'center', fontSize: 25, fontFamily:'NunitoSans', color:'black'}}>Please wait</Text>
+          </View>
+          <View>
+          <Text  style={{width: '50%', textAlign: 'center', fontSize: 25, fontFamily:'NunitoSans', color:'black'}}>We are calculating your plate</Text>
+          </View>
         </Container>
 
       );
